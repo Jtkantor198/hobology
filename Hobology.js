0bbb5eaedfa014e278123571cf94aaa7c2f37d4c
Jtkantor198 <jtkantor198@gmail.com>
2015-07-15 07:51:32 -0400

    Version 2: CSS3 Transitions in progress

--- Hobology.js
+++ Hobology.js
@@ -1,2 +1,11 @@
 /*Write dynamic css to transition to box upon click*/
 /*After click, apply scrolling handler*/
+recentSection = document.getElementById("main-recent");
+mainSectionContainer = document.getElementById("main-section-container");
+sectionContainer = document.getElementById("section-container");
+recentSection.onclick = function(){
+	/*mainSectionContainer.className += " main-section-container-after-click";
+	sectionContainer.className += " section-container-after-click";*/
+	mainSectionContainer.style.margin = "1vh auto 0 calc(50vw - 2em);";
+	sectionContainer.style = "margin: 0vh 0 0 0;";
+}
