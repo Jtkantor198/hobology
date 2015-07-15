9601d8c84041820f7dac3e3f9bc9add493887000
nyarmith <powah.serge@gmail.com>
2015-07-15 18:13:35 -0400

    wow such change

--- Hobology.js
+++ Hobology.js
@@ -1,13 +1,39 @@
 /*Write dynamic css to transition to box upon click*/
 /*After click, apply scrolling handler*/
 recentSection = document.getElementById("main-recent");
-mainSectionContainer = document.getElementsByClassName("main-section-container");
-sectionContainer = document.getElementsByClassName("section-container");
+mainSectionContainer = document.getElementsByClassName("main-section-container")[0];
+sectionContainer = document.getElementsByClassName("section-container")[0];
+
+//Getting the elements to assign handlers to
+recent      = document.getElementById("main-recent");
+projects    = document.getElementById("main-projects");
+blog        = document.getElementById("main-blog");
+readroll    = document.getElementById("main-readroll");
+
+recent.onclick  = function(){
+	mainSectionContainer.className += " recent-active";
+	sectionContainer.className += " main-sect-active";
+};
+projects.onclick= function(){
+	mainSectionContainer.className += " projects-active";
+	sectionContainer.className += " main-sect-active";
+};
+blog.onclick    = function(){
+	mainSectionContainer.className += " blog-active";
+	sectionContainer.className += " main-sect-active";
+};
+readroll.onclick= function(){
+	mainSectionContainer.className += " readroll-active";
+	sectionContainer.className += " main-sect-active";
+};
+
+
+/*
 recentSection.onclick = function(){
-	/*mainSectionContainer.className += " main-section-container-after-click";
-	sectionContainer.className += " section-container-after-click";*/
-	mainSectionContainer[0].className += " sect-active";
-	sectionContainer[0].className += " main-sect-active";
+    mainSectionContainer.addEventListener("transitionend", function() {alert("transition ended!");});
+	mainSectionContainer.className += " sect-active";
+    sectionContainer.addEventListener("transitionend", function() {alert("transition ended!");});
+	sectionContainer.className += " main-sect-active";
 }
 
 function transition(element, property, value, length){
@@ -21,3 +47,4 @@ function transition(element, property, value, length){
 function scrollingHandler(){
 	
 }
+*/
