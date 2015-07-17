155443725540167eabd9e7ab94f4e3249d96e9b6
nyarmith <powah.serge@gmail.com>
2015-07-16 20:23:54 -0400

    Made anonymous function maker to generalize handlers

--- Hobology.js
+++ Hobology.js
@@ -28,50 +28,30 @@ readroll    = document.getElementById("main-readroll");
 
 //The active classes we switch between
 class_list = ["recent-active", "projects-active","blog-active","readroll-active"];
+tab_ele_list   = [recent, projects, blog, readroll];
 
 //assigning handlers to add classes with psuedoselectors to initiate transtions
 var clicked = false;
-recent.onclick  = function(){
-    if (clicked == false){
-      startScrolling();
-      cliked = true;
-    }
-    for (var item in class_list)
-        removeClass(mainSectionContainer, class_list[item]);
-    addClass(mainSectionContainer, class_list[0]);   //Our tab focus
-	addClass(sectionContainer, "main-sect-active");  //Our opening sweep
-};
-projects.onclick= function(){
-    if (clicked == false){
-      startScrolling();
-      cliked = true;
-    }
-    for (var item in class_list)
-        removeClass(mainSectionContainer, class_list[item]);
-    addClass(mainSectionContainer, class_list[1]);   //Our tab focus
-	addClass(sectionContainer, "main-sect-active");  //Our opening sweep
-};
-blog.onclick    = function(){
-    if (clicked == false){
-      startScrolling();
-      cliked = true;
-    }
-    for (var item in class_list)
-        removeClass(mainSectionContainer, class_list[item]);
-    addClass(mainSectionContainer, class_list[2]);   //Our tab focus
-	addClass(sectionContainer, "main-sect-active");  //Our opening sweep
-};
-readroll.onclick= function(){
-    if (clicked == false){
-      startScrolling();
-      cliked = true;
-    }
-    for (var item in class_list)
-        removeClass(mainSectionContainer, class_list[item]);
-    addClass(mainSectionContainer, class_list[3]);   //Our tab focus
-	addClass(sectionContainer, "main-sect-active");  //Our opening sweep
-};
 
+function tabHandler(value){
+    return function(){
+        if (clicked == false){
+          startScrolling();
+          clicked = true;
+        }
+        for (var i=0; i<class_list.length; i++) {
+            removeClass(mainSectionContainer, class_list[i]);
+            addClass(tab_ele_list[i],"tab-passive");
+        }
+        removeClass(tab_ele_list[value],"tab-passive");
+        addClass(mainSectionContainer, class_list[value]);   //Our tab focus
+	    addClass(sectionContainer, "main-sect-active");  //Our opening sweep
+    };
+}
+
+for (var i=0; i<tab_ele_list.length; i++){
+    tab_ele_list[i].onclick=tabHandler(i);
+}
 
 /*--------Assigning hadlers to build about and contact pages, and switch between them--------*/
 var mainTopRight = document.getElementsByClassName("main-top-right")[0];
